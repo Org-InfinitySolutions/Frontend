3e4a2b22a6c2783da02a58e4c8ff3ab5ecc59044
MarcoAntonioMS <marco.souza@sptech.school>
2025-03-17 11:30:42 -0300

    Integração do JavaScriptUtils com json-server

--- JavaScriptUtils/JavaScriptUtils.js
+++ JavaScriptUtils/JavaScriptUtils.js
@@ -62,70 +62,107 @@ function formatarRegistroGeral() {
     this.value = registroGeral;
 }
 
-function validarFormulario() {
-    let cpf = document.getElementById('cpf').value;
-    let cnpj = document.getElementById('cnpj').value;
-    let telefone = document.getElementById('telefone').value;
-    let registroGeral = document.getElementById('registroGeral').value;
-    let email = document.getElementById('email').value;
-  
+        function validarFormulario() {
+            let cpf = document.getElementById('cpf').value;
+            let cnpj = document.getElementById('cnpj').value;
+            let telefone = document.getElementById('telefone').value;
+            let registroGeral = document.getElementById('registroGeral').value;
+            let email = document.getElementById('email').value;
+          
 
-    let cpfError = document.getElementById('cpf-error');
-    let cnpjError = document.getElementById('cnpj-error');
-    let telefoneError = document.getElementById('telefone-error');
-    let registroGeralError = document.getElementById('registroGeral-error');
-    let emailError = document.getElementById('email-error');
+            let cpfError = document.getElementById('cpf-error');
+            let cnpjError = document.getElementById('cnpj-error');
+            let telefoneError = document.getElementById('telefone-error');
+            let registroGeralError = document.getElementById('registroGeral-error');
+            let emailError = document.getElementById('email-error');
 
-    cpfError.innerHTML = '';
-    cnpjError.innerHTML = '';
-    telefoneError.innerHTML = '';
-    registroGeralError.innerHTML = '';
-    emailError.innerHTML = '';
+            cpfError.innerHTML = '';
+            cnpjError.innerHTML = '';
+            telefoneError.innerHTML = '';
+            registroGeralError.innerHTML = '';
+            emailError.innerHTML = '';
 
-    if (cpf.length > 0 && cpf.length < 14) {
-        cpfError.innerHTML = 'CPF inválido. Verifique o formato.do';
+            let isValid = true;
+
+            if (cpf.length > 0 && cpf.length < 14) {
+                cpfError.innerHTML = 'CPF inválido. Verifique o formato.do';
+                isValid = false;
+            }
+
+            if (cnpj.length > 0 && cnpj.length < 18) {
+                cnpjError.innerHTML = 'CNPJ inválido. Verifique o formato.';
+                isValid = false;
+            }
+
+            if (telefone.length > 0 && telefone.length < 15) {
+                telefoneError.innerHTML = 'Telefone inválido. Verifique o formato.';
+                isValid = false;
+            }
+
+            if (registroGeral.length > 0 && registroGeral.length < 11) {
+                registroGeralError.innerHTML = 'RG inválido. Verifique o formato.';
+                isValid = false;
+            }
+
+            if (cpf === "") {
+                cpfError.innerHTML = 'Campo obrigatório';
+                isValid = false;
+            }
+
+            if (cnpj === "") {
+                cnpjError.innerHTML = 'Campo obrigatório';
+                isValid = false;
+            }
+
+            if (telefone === "") {
+                telefoneError.innerHTML = 'Campo obrigatório';
+                isValid = false;
+            }
+
+            if (registroGeral === "") {
+                registroGeralError.innerHTML = 'Campo obrigatório';
+                isValid = false;
+            }
+
+            if (email === "") {
+                emailError.innerHTML = 'Campo obrigatório';
+                isValid = false;
+            }
+            else {
+                
+            let arrobaIndex = email.indexOf('@');
+            let pontoIndex = email.indexOf('.', arrobaIndex);
+
+            if (arrobaIndex === -1 || pontoIndex === -1 || pontoIndex < arrobaIndex) {
+                emailError.innerHTML = 'E-mail inválido. Verifique o formato.';
+                isValid = false;
+            }
+        }
+        if(isValid){
+            enviarDados();
+        }
     }
+   
+function enviarDados() {
+    let usuario = {
+        cpf: document.getElementById("cpf").value,
+        cnpj: document.getElementById("cnpj").value,
+        telefone: document.getElementById("telefone").value,
+        registroGeral: document.getElementById("registroGeral").value,
+        email: document.getElementById("email").value
+    };
 
-    if (cnpj.length > 0 && cnpj.length < 18) {
-        cnpjError.innerHTML = 'CNPJ inválido. Verifique o formato.';
-    }
-
-    if (telefone.length > 0 && telefone.length < 15) {
-        telefoneError.innerHTML = 'Telefone inválido. Verifique o formato.';
-    }
-
-    if (registroGeral.length > 0 && registroGeral.length < 11) {
-        registroGeralError.innerHTML = 'RG inválido. Verifique o formato.';
-    }
-
-    if (cpf === "") {
-        cpfError.innerHTML = 'Campo obrigatório';
-    }
-
-    if (cnpj === "") {
-        cnpjError.innerHTML = 'Campo obrigatório';
-    }
-
-    if (telefone === "") {
-        telefoneError.innerHTML = 'Campo obrigatório';
-    }
-
-    if (registroGeral === "") {
-        registroGeralError.innerHTML = 'Campo obrigatório';
-    }
-
-    if (email === "") {
-        emailError.innerHTML = 'Campo obrigatório';
-    }
-    else {
-        
-    let arrobaIndex = email.indexOf('@');
-    let pontoIndex = email.indexOf('.', arrobaIndex);
-
-    if (arrobaIndex === -1 || pontoIndex === -1 || pontoIndex < arrobaIndex) {
-        emailError.innerHTML = 'E-mail inválido. Verifique o formato.';
-    }
-
-}
+    fetch("http://localhost:3000/usuarios", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(usuario)
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log("Usuário cadastrado com sucesso:", data);
+        alert("Usuário cadastrado com sucesso!");
+    })
 }
 
