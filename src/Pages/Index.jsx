64830db399453b37ad8da53b98ad81c2ca2cfc30
Gabriela <gabriela.ferreira@sptech.school>
2025-04-18 14:02:11 -0300

    Primeira versão, vídeo e início do rodapé

--- src/Pages/Index.jsx
+++ src/Pages/Index.jsx
@@ -1,6 +1,11 @@
 import './Index.css';
 import videoFundo from '../assets/video-institucional.mp4';
 
+import { Carousel } from "../components/Carrossel/Carousel"
+import {CarouselItem} from "../components/Carrossel/CarouselItem"
+import { DivCarouselItem } from '../components/Carrossel/style';    
+    
+
 function Index() {
     return (
         <section className="container">
@@ -20,14 +25,35 @@ function Index() {
             <h1>NOVA LOCAÇÕES</h1>
             <div className="barra"></div>
             </section>
-
         </section>
 
 
-        <section className="container-conteudo">
-                <p>Explore nossos produtos e serviços.</p>
+        <section className="container-sobre-nos">
+                <h2>Sobre nós</h2>
+                <p> <b> Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores, asperiores ipsam autem et voluptatem delectus
+                     quos itaque dolore, minima dolor fugiat consequuntur? Possimus quo iusto assumenda eos labore reiciendis in?
+                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias quaerat obcaecati eveniet provident optio adipisci</b>
+                     vitae maiores numquam culpa quam vero, illum iusto voluptate aliquam cum eaque debitis, voluptas amet!
+                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo aut fugit cupiditate tempora provident laborum molestias nam voluptas libero,
+                      nobis fugiat quasi repellendus eum, quidem sapiente quisquam excepturi corporis assumenda.</p>
+        </section>
+
+        <section className="container-servicos">
+                <h2>Serviços</h2>
+                      <section className="container-carrosel" >
+                     
+                    <Carousel>
+                        <CarouselItem>Item 1</CarouselItem>
+                        <CarouselItem>Item 2</CarouselItem>
+                        <CarouselItem>Item 3</CarouselItem>
+                        <CarouselItem>Item 4</CarouselItem>
+                        <CarouselItem>Item 5</CarouselItem>
+                    </Carousel>
+                      </section>
         </section>
         </section>
     );
 }
+
+
 export { Index };
---
+++ src/components/Carrossel/Carousel.jsx
@@ -0,0 +1,75 @@
+// Caso tenha erro de esse comando npm install styled-components
+
+import React, { useEffect, useState } from "react"
+import { CarouselContainer, Indicadores, Inner, Next, Prev } from "./style"
+import { AiOutlineDoubleLeft } from "react-icons/ai"; 
+import { AiOutlineDoubleRight } from "react-icons/ai"; // de npm install react-icons
+import { useSwipeable } from "react-swipeable"; // de npm install react-swipeable
+
+export const Carousel=({children})=> {
+
+const [activeIndex, setActiveIndex] = useState(0);
+const [paused, setPaused] =useState(false);
+
+const updateIndex = (newIndex) =>{
+    if(newIndex <0){
+    newIndex = 0;
+    }
+    else if (newIndex>= React.Children.count(children)){
+        newIndex = React.Children.count(children) -1;
+    }
+    setActiveIndex(newIndex)
+};
+
+useEffect(()=>{
+    const interval = setInterval(()=>{
+        if(!paused){
+            updateIndex(activeIndex +1);
+        }
+    }, 6000);
+    return()=>{
+        if(interval){
+            clearInterval(interval)
+        }
+    }
+})
+
+const handlers = useSwipeable({
+    onSwipedLeft:()=>updateIndex(activeIndex+1),
+    onSwipedRight:()=>updateIndex(activeIndex-1)
+})
+    return (
+  <>
+  <CarouselContainer
+   {...handlers}
+  onMouseEnter={()=> setPaused(true)}
+  onMouseLeave={()=> setPaused(false)}
+  >
+  <Indicadores>
+        <Prev onClick={()=>{
+            updateIndex(activeIndex -1)
+        }}>
+            <img src="./src/assets/setaEsquerda.png" alt="seta esquerda" //Como alterar tamanho da imagem?   
+            style={{ width: "30px", height: "30px" }}
+            />
+        </Prev>
+        <Inner style={{transform: `translateX(-${activeIndex*32}%)`}}>
+        {React.Children.map(children,(child, index)=>{
+            return React.cloneElement(child, {width:"25%"})
+        })}
+        </Inner>
+        
+        <Next onClick={()=>{
+            updateIndex(activeIndex +1)
+        }}>
+             <img src="./src/assets/setaDireita.png" alt="seta direita"
+              style={{ width: "30px", height: "30px" }}
+            />
+        </Next>
+    </Indicadores>
+  </CarouselContainer>
+  </>
+      
+    )
+  }
+  
